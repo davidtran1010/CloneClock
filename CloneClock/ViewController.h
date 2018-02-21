2ebbc1a7ca064bfdb7b554021dcec20b110e3022
DavidTran <davidtran@DavidTrans-MacBook-Pro.local>
2018-02-21 10:07:16 +0700

    Initial Commit

---
+++ CloneClock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CloneClock
+//
+//  Created by DavidTran on 2/21/18.
+//  Copyright © 2018 DavidTran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
